570d13e9be5d21d434601b2e2aa321b1a7728a2b
kissmaelis <kissmaelis@proton.me>
2026-02-02 10:34:23 +0100

    git clone starter kit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-3xl font-bold text-gray-800">
+          Vite ⚡ + React ⚛️ + Tailwind CSS 🎨
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
