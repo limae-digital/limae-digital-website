c825cfde4e38fe3141ac7472a31208fe94a0ff51
kissmaelis <kissmaelis@proton.me>
2026-02-02 11:20:02 +0100

    upgrade so it match limae digital

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-3xl font-bold text-gray-800">
-          Vite ⚡ + React ⚛️ + Tailwind CSS 🎨
-        </h1>
-      </div>
-    </>
+    <div className="min-h-screen flex flex-col relative">
+      <main className="flex-1 flex items-center justify-center">
+        <img
+          src="/limaedigital.svg"
+          alt="limaedigital logo"
+          className="w-36 h-36 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-80 lg:h-80"
+        />
+      </main>
+      <footer className="fixed bottom-0 left-0 right-0 py-4 text-center text-sm text-gray-600 bg-white">Made with ❤️ by LIMAE DIGITAL</footer>
+    </div>
   );
 }
 
